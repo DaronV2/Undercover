2302c9c1905dbc37bcd1ccdc1c4987c509fa14c5
DaronV2 <darkenciel31@gmail.com>
2024-04-04 22:36:17 +0200

    Premier commit

--- app/src/main/java/com/example/undercover/MainActivity.kt
+++ app/src/main/java/com/example/undercover/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.undercover
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import com.example.undercover.ui.theme.UndercoverTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.graphics.Color.Companion.Black
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,18 +42,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Surface(color = Color.DarkGray) {
+        AffichageLogo()
+   }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    UndercoverTheme {
-        Greeting("Android")
+fun GreetingImage(modifier: Modifier = Modifier) {
+    /*val image = painterResource(id = R.drawable.avdol)
+    Image(painter = image, contentDescription = "avdoooolu", modifier.padding(
+        start = 20.dp
+    ))*/
+}
+
+@Composable
+fun AffichageLogo(){
+    Column {
+        var modifier = Modifier
+            .padding(15.dp)
+            .fillMaxWidth()
     }
 }
